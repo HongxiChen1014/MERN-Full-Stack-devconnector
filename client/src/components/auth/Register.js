ce52d79f692554f9e1451266efe231dcac3aafa5
HongxiChen1014 <hongxi_chen2020@163.com>
2022-07-19 02:00:34 -0700

    update auth

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
-import React, {Link, useState} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -17,25 +18,26 @@ const Register = () => {
         if(password !== password2) {
             console.log('Passwords do not match');
         } else {
-            const newUser = {
-                name,
-                email,
-                password
-            }
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
+          console.log("SUCCESS");
+            // const newUser = {
+            //     name,
+            //     email,
+            //     password
+            // }
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     }
 
-                const body = JSON.stringify(newUser);
+            //     const body = JSON.stringify(newUser);
                 
-                const res = await axios.post('/api/users', body, config);
-                console.log(res.data);
-            } catch(err) {
-                console.error(err.response.data);
-            }
+            //     const res = await axios.post('/api/users', body, config);
+            //     console.log(res.data);
+            // } catch(err) {
+            //     console.error(err.response.data);
+            // }
         }
     }
 
@@ -89,7 +91,7 @@ const Register = () => {
           <input type="submit" className="btn btn-primary" value="Register" />
         </form>
         <p className="my-1">
-            Already have an account? <a href="login.html">Sign In</a>
+            Already have an account? <Link to="/login">Sign In</Link>
         </p>
       </section>
 )};
