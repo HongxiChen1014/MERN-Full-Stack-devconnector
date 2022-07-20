2245eb2e17a1cb0799f6a98d5b62163bb5aa6189
HongxiChen1014 <hongxi_chen2020@163.com>
2022-07-20 03:50:55 -0700

    add login authentication redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,28 +22,13 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Passwords do not match", "danger");
     } else {
-      console.log("SUCCESS");
-      // const newUser = {
-      //     name,
-      //     email,
-      //     password
-      // }
-      // try {
-      //     const config = {
-      //         headers: {
-      //             'Content-Type': 'application/json'
-      //         }
-      //     }
-
-      //     const body = JSON.stringify(newUser);
-
-      //     const res = await axios.post('/api/users', body, config);
-      //     console.log(res.data);
-      // } catch(err) {
-      //     console.error(err.response.data);
-      // }
+      register({ name, email, password });
     }
   };
+
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
 
   return (
     <section className="container">
@@ -102,6 +88,12 @@ const Register = ({ setAlert }) => {
 
 Register.protoTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
