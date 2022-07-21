b5e53b6cf77b4d215aad1f0424cb32cfaa14df4e
HongxiChen1014 <hongxi_chen2020@163.com>
2022-07-21 00:10:17 -0700

    delete Experience Education function added

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { setAlert } from "./alert";
 
 import {
+    ACCOUNT_DELETED,
+    CLEAR_PROFILE,
     GET_PROFILE,
     PROFILE_ERROR,
     UPDATE_PROFILE
@@ -112,5 +114,61 @@ export const addEducation = (formData, navigate) => async dispatch => {
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
         });
+    }
+}
+
+// Delete Experience
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`);
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+// Delete Education
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+// Delete account & profile
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure? This cannot be undone!')) {
+        try {
+            const res = await axios.delete('/api/profile');
+
+            dispatch({ type: CLEAR_PROFILE });
+            dispatch({ type: ACCOUNT_DELETED });
+
+            dispatch(setAlert('Your account has been permanantly deleted'));
+        } catch (err) {
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: { msg: err.response.statusText, status: err.response.status }
+            });
+        }
     }
 }
