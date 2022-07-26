f1783a02d1084879565c2d954e99653a78a5fd26
HongxiChen1014 <hongxi_chen2020@163.com>
2022-07-26 14:11:25 -0700

    add post display

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST } from "../actions/types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST, ADD_COMMENT, REMOVE_COMMENT } from "../actions/types";
 
 
 const initialState = {
@@ -49,6 +49,22 @@ export default function (state = initialState, action) {
                     ...post, likes:
                         payload.likes
                 } : post)
+            }
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: { ...state.post, comments: payload },
+                loading: false
+            }
+
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(comment => comment._id !== payload)
+                },
+                loading: false
             }
         default:
             return state;
